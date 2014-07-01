17e36d5baf6e351cc28faec6bf4342a8127fb501
Fabricio Orrala <orrala@celmedia.com>
2014-07-01 17:23:49 -0500

    menu principal

--- src/Celmedia/Toyocosta/VehiculosBundle/Resources/public/js/script.toyocosta.js
+++ src/Celmedia/Toyocosta/VehiculosBundle/Resources/public/js/script.toyocosta.js
@@ -8,9 +8,22 @@ $(function() {
 	});
 });
 
+$(window).bind('scroll', function() {
+	if ($(window).scrollTop() > 50) {
+		$('.sombra-toyocosta').addClass('bajar');
+	}
+	else {
+		$('.sombra-toyocosta').removeClass('bajar');
+	}
+});
 
 $(document).ready(function(){
 
+	$('ul.nav > li').click(function (e) {
+        e.preventDefault();
+        $('ul.nav > li').removeClass('active');
+        $(this).addClass('active');
+    });
 
 
 	// $('.miniaturas.active').find(".img-indicator").show();
