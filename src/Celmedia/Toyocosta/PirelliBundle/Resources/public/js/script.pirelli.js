e826546f70cb84af749853fb75cab7ef03839bff
Fabricio Orrala <orrala@celmedia.com>
2014-07-21 18:27:45 -0500

    ajustes

--- src/Celmedia/Toyocosta/PirelliBundle/Resources/public/js/script.pirelli.js
+++ src/Celmedia/Toyocosta/PirelliBundle/Resources/public/js/script.pirelli.js
@@ -16,101 +16,101 @@ $(document).ready(function(){
       $('.selectcmb').selectbox();
 });
 
-  function validarDatos() {
-  
-    $("#form-pirelli").validate({
-          debug: true,
-          submitHandler: function (form) {
-              var parametros = {
-                  nombre: $("#inputNombre").val(),
-                  apellido: $("#inputApellido").val(),
-                  telefono: $("#inputTelefono").val(),
-                  celular: $("#inputCelular").val(),
-                  email: $("#inputEmail").val(),
-                  cedula: $("#inputCedula").val(),
-                  comentario: $("#inputComentario").val(),
-                  llanta: $("#llanta_id").val()
-              }
+function validarDatos() {
 
-              $.ajax({
-                  url: Routing.generate('pirelli_envio_info'),
-                  type: 'POST',
-                  async: true,
-                  data: parametros,
-                  success: function (respuesta) {
-                      // alert(respuesta);
-                      if (respuesta == "ok") {
-                          // alert("Su pedido de informacion fue enviado exitosamente");
-                          // document.getElementById("form-pirelli").reset();
-                          window.location = Routing.generate('pirelli_mensaje');
-                      } else {
-                          // error
-                      }
+  $("#form-pirelli").validate({
+        debug: true,
+        submitHandler: function (form) {
+            var parametros = {
+                nombre: $("#inputNombre").val(),
+                apellido: $("#inputApellido").val(),
+                telefono: $("#inputTelefono").val(),
+                celular: $("#inputCelular").val(),
+                email: $("#inputEmail").val(),
+                cedula: $("#inputCedula").val(),
+                comentario: $("#inputComentario").val(),
+                llanta: $("#llanta_id").val()
+            }
 
-                  }, 
-                  error: function (error) {
-                    console.log("ERROR: " + error);
-                  }
-              });
+            $.ajax({
+                url: Routing.generate('pirelli_envio_info'),
+                type: 'POST',
+                async: true,
+                data: parametros,
+                success: function (respuesta) {
+                    // alert(respuesta);
+                    if (respuesta == "ok") {
+                        // alert("Su pedido de informacion fue enviado exitosamente");
+                        // document.getElementById("form-pirelli").reset();
+                        window.location = Routing.generate('pirelli_mensaje');
+                    } else {
+                        // error
+                    }
 
-          },
-          rules: {
-              inputNombre: {
-                  required: true
-              },
-              inputEmail: {
-                  required: true,
-                  email: true
-              },
-              inputApellido: {
-                  required: true
-              },
-              inputTelefono: {
-                  required:true,
-                  minlength:7,
-                  maxlength:15,
-                  number:true
-              },
-              inputCelular: {
-                  required:true,
-                  minlength:10,
-                  maxlength:10,
-                  number:true
-              },
-              inputCedula: {
-                  required:true,
-                  minlength:10,
-                  maxlength:10,
-                  number:true
-              },
-              inputComentario: {
-                  required: true
-              }
+                }, 
+                error: function (error) {
+                  console.log("ERROR: " + error);
+                }
+            });
 
-          },
-          showErrors: function (errorMap, errorList) {
-               // Clean up any tooltips for valid elements
-              $.each(this.validElements(), function (index, element) {
-                  var $element = $(element);
+        },
+        rules: {
+            inputNombre: {
+                required: true
+            },
+            inputEmail: {
+                required: true,
+                email: true
+            },
+            inputApellido: {
+                required: true
+            },
+            inputTelefono: {
+                required:true,
+                minlength:7,
+                maxlength:15,
+                number:true
+            },
+            inputCelular: {
+                required:true,
+                minlength:10,
+                maxlength:10,
+                number:true
+            },
+            inputCedula: {
+                required:true,
+                minlength:10,
+                maxlength:10,
+                number:true
+            },
+            inputComentario: {
+                required: true
+            }
 
-                  $element.data("title", "") // Clear the title - there is no error associated anymore
-                      .removeClass("error")
-                      .tooltip("destroy");
-              });
+        },
+        showErrors: function (errorMap, errorList) {
+             // Clean up any tooltips for valid elements
+            $.each(this.validElements(), function (index, element) {
+                var $element = $(element);
 
-              // Create new tooltips for invalid elements
-              $.each(errorList, function (index, error) {
-                  var $element = $(error.element);
+                $element.data("title", "") // Clear the title - there is no error associated anymore
+                    .removeClass("error")
+                    .tooltip("destroy");
+            });
 
-                  $element.tooltip("destroy") // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
-                      .data("title", error.message)
-                      .addClass("error")
-                      .tooltip(); // Create a new tooltip based on the error messsage we just set in the title
-                  });
+            // Create new tooltips for invalid elements
+            $.each(errorList, function (index, error) {
+                var $element = $(error.element);
 
-          }
-       });
+                $element.tooltip("destroy") // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
+                    .data("title", error.message)
+                    .addClass("error")
+                    .tooltip(); // Create a new tooltip based on the error messsage we just set in the title
+                });
+
+        }
+     });
 
 
 
-  }
+}
