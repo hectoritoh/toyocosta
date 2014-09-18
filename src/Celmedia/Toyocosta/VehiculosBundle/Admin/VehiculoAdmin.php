4f0f59cbf1311cd7e90ac81f4db59473d834a4d5
yuri cosquillo <yurixy25@gmail.com>
2014-09-17 19:38:18 -0500

    sacar with end

--- src/Celmedia/Toyocosta/VehiculosBundle/Admin/VehiculoAdmin.php
+++ src/Celmedia/Toyocosta/VehiculosBundle/Admin/VehiculoAdmin.php
@@ -221,31 +221,25 @@ class VehiculoAdmin extends Admin
             //          'inline' => 'standard',
             //      ))
             // ->end()
-            ->with('Colores del Vehiculo')
-                ->add('colores', 'sonata_type_collection', array(
-                     'by_reference' => false,
-                           // Prevents the "Delete" option from being displayed
-                     'type_options' => array('delete' => false)) , array(
-                     'edit' => 'inline',
-                     'inline' => 'standard',
-                 ))
-            ->end()
-            ->with('Modelos del Vehiculo')
-                ->add('modelos', 'sonata_type_collection', array(
-                     'by_reference' => false,
-                     'type_options' => array('delete' => false)) , array(
-                     'edit' => 'inline',
-                     'inline' => 'standard',
-                 ))
-            ->end()
-            ->with('Especificaciones del Vehiculo')
-                ->add('especificaciones', 'sonata_type_collection', array(
+            ->add('colores', 'sonata_type_collection', array(
+                 'by_reference' => false,
+                       // Prevents the "Delete" option from being displayed
+                 'type_options' => array('delete' => false)) , array(
+                 'edit' => 'inline',
+                 'inline' => 'standard',
+             ))
+            ->add('modelos', 'sonata_type_collection', array(
+                 'by_reference' => false,
+                 'type_options' => array('delete' => false)) , array(
+                 'edit' => 'inline',
+                 'inline' => 'standard',
+             ))
+            ->add('especificaciones', 'sonata_type_collection', array(
                      'by_reference' => false,
                      'type_options' => array('delete' => false)) , array(
                      'edit' => 'inline',
                      'inline' => 'standard',
-                 ))
-            ->end()            
+                 ))           
             ->setHelps(array(
                     'precio' => 'Precio sin IVA',
                     'precio_neto' => 'Precio con Iva',
