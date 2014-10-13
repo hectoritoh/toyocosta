49a8722cf553d505a167de8e44b13360d98a7a0f
yuri cosquillo <yurixy25@gmail.com>
2014-10-13 18:18:03 -0500

    ajustes en el admin y archivos.

---
+++ src/Celmedia/Toyocosta/VehiculosBundle/Controller/SlideVehiculosAdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Celmedia\Toyocosta\VehiculosBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class SlideVehiculosAdminController extends CRUDController
+{
+
+
+
+}
