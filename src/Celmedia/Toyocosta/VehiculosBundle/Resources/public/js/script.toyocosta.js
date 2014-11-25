7cb0d52f98f918711205bb1cf9c0d04d67837984
Yuri Cosquillo <ycosquillo@celmedia.com>
2014-11-25 17:41:40 -0500

    cargador toyocosta

--- src/Celmedia/Toyocosta/VehiculosBundle/Resources/public/js/script.toyocosta.js
+++ src/Celmedia/Toyocosta/VehiculosBundle/Resources/public/js/script.toyocosta.js
@@ -265,6 +265,19 @@ var seleccionado = false;
 
 $(document).ready(function(){
 
+  $("body").queryLoader2({
+    barColor: "#df192b",
+    backgroundColor: "#fff",
+        // percentage: true,
+        barHeight: 1,
+        completeAnimation: "grow",
+        minimumTime: 100,
+        onLoadComplete:function(){
+          
+
+        }
+  });
+
 
   $('.navbar-nav-vehiculos').find('li').removeClass("active");
 
